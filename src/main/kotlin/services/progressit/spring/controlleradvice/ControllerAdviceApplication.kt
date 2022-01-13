b59183a1d139c2ddaa8c892a2280f48c4e9fd832
Sebastian Stein <seb.kde@hpfsc.de>
2022-01-13 19:18:58 +0100

    inital import

---
+++ src/main/kotlin/services/progressit/spring/controlleradvice/ControllerAdviceApplication.kt
@@ -0,0 +1,11 @@
+package services.progressit.spring.controlleradvice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ControllerAdviceApplication
+
+fun main(args: Array<String>) {
+	runApplication<ControllerAdviceApplication>(*args)
+}
---
+++ src/test/kotlin/services/progressit/spring/controlleradvice/ControllerAdviceApplicationTests.kt
@@ -0,0 +1,13 @@
+package services.progressit.spring.controlleradvice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ControllerAdviceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
